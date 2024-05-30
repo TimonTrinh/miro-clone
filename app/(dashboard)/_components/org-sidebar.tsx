b03dc0b8598b8967cbd4ae977dce89e59967297a
Timon-CHP <naturalgroup.vn@gmail.com>
2024-05-30 18:24:51 +0700

    organizing boards

--- app/(dashboard)/_components/org-sidebar.tsx
+++ app/(dashboard)/_components/org-sidebar.tsx
@@ -6,11 +6,16 @@ import { Poppins } from "next/font/google";
 import { cn } from "@/lib/utils";
 import { OrganizationSwitcher } from "@clerk/nextjs";
 import { Button } from "@/components/ui/button";
-import { LayoutDashboard } from "lucide-react";
+import { LayoutDashboard, Star } from "lucide-react";
+import {useSearchParams} from "next/navigation";
 
 const font = Poppins({ subsets: ["latin"], weight: ["600"]});
 
 const OrgSidebar = () => {
+
+  const searchParams = useSearchParams();
+  const isFavorites = searchParams.get("favorites");
+
   return (
     <div className="hidden lg:flex flex-col space-y-6 w-[206px] pl-5 pt-5">
       <Link href="/">
@@ -45,10 +50,18 @@ const OrgSidebar = () => {
         }}
       />
       <div className="space-y-1 w-full">
-        <Button asChild size="lg" className="font-normal justify-start px-2 w-full">
+        <Button variant={isFavorites ? "ghost": "secondary"} asChild size="lg" className="font-normal justify-start px-2 w-full">
           <Link href="/" >
             <LayoutDashboard className="h-4 w-4 mr-2"/>
             Team boards
+          </Link>
+        </Button>
+        <Button variant={isFavorites ? "secondary": "ghost"} asChild size="lg" className="font-normal justify-start px-2 w-full">
+          <Link href={{pathname: "/", 
+            query: {favorites: true} 
+          }} >
+            <Star className="h-4 w-4 mr-2"/>
+            Favorite boards
           </Link>
         </Button>
       </div>
