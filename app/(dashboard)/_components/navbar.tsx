4759de1987f6a6a908ef784e458f4d396d5c1736
Timon-CHP <naturalgroup.vn@gmail.com>
2024-05-30 20:45:54 +0700

    Search bar on desktop

--- app/(dashboard)/_components/navbar.tsx
+++ app/(dashboard)/_components/navbar.tsx
@@ -2,12 +2,13 @@
 
 import { UserButton } from '@clerk/nextjs';
 import React from 'react'
+import { SearchInput } from './search-input';
 
 export const Navbar = () => {
   return (
-    <div className="flex items-center gap-x-4 p-5 bg-green-500">
+    <div className="flex items-center gap-x-4 p-5">
         <div className="hidden lg:flex lg:flex-1">
-            {/* <Search /> */}
+            <SearchInput />
         </div>
         <div className="block p-5"><UserButton /></div>
     </div>
