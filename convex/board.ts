a3bfddec482141dab699fee30762795643450c7d
Timon-CHP <naturalgroup.vn@gmail.com>
2024-06-27 21:17:45 +0700

    renaming board

--- convex/board.ts
+++ convex/board.ts
@@ -62,3 +62,37 @@ export const remove = mutation({
         await ctx.db.delete( args.id);
     }
 })
+
+export const rename = mutation({ 
+    args: {
+        id: v.id("boards"),
+        title: v.string()
+    }, 
+    handler: async (ctx, args) => {
+        const title = args.title.trim();
+        if (!title) {
+            throw new Error("Title is required");
+        }
+        if (title.length > 60) {
+            throw new Error("Title cannot be longer than 60 characters.");
+        }
+
+        const identity = await ctx.auth.getUserIdentity();
+        if (!identity) {
+            throw new Error("Unauthorized");
+        }
+
+        const board = await ctx.db.get(args.id);
+        if (!board) {
+            throw new Error("Board not found");
+        }
+
+        if (board.orgId !== identity.subject) {
+            throw new Error("User is not board's owner");
+        }
+
+        await ctx.db.patch(args.id, {
+            title: args.title
+        });
+    }
+})
