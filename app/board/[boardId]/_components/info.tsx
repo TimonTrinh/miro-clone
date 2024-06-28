906e689d131bcc189110edb2ae09ff59fef1ef38
Timon-CHP <naturalgroup.vn@gmail.com>
2024-06-28 17:03:20 +0700

    Fix bug server-client skeleton component

--- app/board/[boardId]/_components/info.tsx
+++ app/board/[boardId]/_components/info.tsx
@@ -1,4 +1,3 @@
-
 import { Skeleton } from "@/components/ui/skeleton";
 
 interface InfoProps {
@@ -15,7 +14,7 @@ export const Info = ({
     );
 }
 
-Info.Skeleton = function InfoSkeleton() {
+export const InfoSkeleton = () => {
     return (
         <div className="absolute top-2 left-2 rounded-md px-1.5 h-12 flex items-center shadow-md w-[300px] bg-white" >
             <Skeleton className="h-full w-full bg-muted-400"/>
