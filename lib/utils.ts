9bf5c2da2cdd216ae686fadae313a2a2d49dc351
Timon-CHP <naturalgroup.vn@gmail.com>
2024-06-30 15:31:36 +0700

    add function color by connection id.

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,29 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+const COLORS = [
+  "teal",
+  "indigo",
+  "lime",
+  "amber",
+  "emerald",
+  "fuchsia",
+  "violet",
+  "cyan",
+  "sky",
+  "sapphire",
+  "lavender",
+  "rose",
+  "maroon",
+  "olive",
+  "navy",
+  "teal",
+  "mint",
+  "peach",
+  "carrot",
+  "brown"
+];
+export function connectionIdColor(connectionId: number): string { 
+  return COLORS[connectionId % COLORS.length];
+}
