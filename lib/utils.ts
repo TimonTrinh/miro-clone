762e3d787da91676bdb7cb6aab7b50ef8b46500b
Timon-CHP <naturalgroup.vn@gmail.com>
2024-07-01 22:37:31 +0700

    function translate pointer to camera position

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,3 +1,4 @@
+import { Camera } from "@/types/canvas";
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 
@@ -30,3 +31,13 @@ const COLORS = [
 export function connectionIdColor(connectionId: number): string { 
   return COLORS[connectionId % COLORS.length];
 }
+
+export function pointerEventToCanvasPoint(
+  e: React.PointerEvent,
+  camera: Camera,
+) {
+  return {
+    x: Math.round(e.clientX) - camera.x,
+    y: Math.round(e.clientY) - camera.y,
+  };
+};
