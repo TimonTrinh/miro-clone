929fc2863012e9c435773fdb9428bcbb55b9cf3e
Timon-CHP <naturalgroup.vn@gmail.com>
2024-07-03 19:57:14 +0700

    add function resizeBounds

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,4 +1,4 @@
-import { Camera, Color } from "@/types/canvas";
+import { Camera, Color, Point, Side, XYWH } from "@/types/canvas";
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 
@@ -43,4 +43,41 @@ export function pointerEventToCanvasPoint(
 
 export function colorToCss(color: Color) {
   return `#${color.r.toString(16).padStart(2,"0")}${color.g.toString(16).padStart(2,"0")}${color.b.toString(16).padStart(2,"0")}`
+}
+
+const MIN_RESIZE_WIDTH = 10;
+const MIN_RESIZE_HEIGHT = 10;
+export function resizeBounds(
+  bounds: XYWH, 
+  corner: Side, 
+  point: Point): XYWH {
+  
+    const result = {
+    x: bounds.x,
+    y: bounds.y,
+    width: bounds.width,
+    height: bounds.height,
+  };
+
+  if ((corner && Side.Left) === Side.Left) {
+    result.x = Math.min(point.x, bounds.x + bounds.width);
+    result.width = Math.min(MIN_RESIZE_WIDTH,Math.abs(bounds.x + bounds.width - point.x));
+  }
+
+  if ((corner && Side.Right) === Side.Right) {
+    result.x = Math.min(point.x, bounds.x);
+    result.width = Math.min( MIN_RESIZE_WIDTH,Math.abs(point.x - bounds.x));
+  }
+
+  if ((corner && Side.Top) === Side.Top) {
+    result.y = Math.min(point.y, bounds.y + bounds.height);
+    result.height = Math.min(MIN_RESIZE_HEIGHT,Math.abs(bounds.y + bounds.height - point.y));
+  }
+
+  if ((corner && Side.Bottom) === Side.Bottom) {
+    result.y = Math.min(point.y, bounds.y);
+    result.height = Math.min(MIN_RESIZE_HEIGHT,Math.abs(point.y - bounds.y));
+  }
+
+  return result;
 }
